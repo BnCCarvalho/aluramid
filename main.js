fa2adeeecad2dc925ad89a268c58a692f41cf24b
BnCCarvalho <99913468+BnCCarvalho@users.noreply.github.com>
2023-08-21 15:00:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    console.log(`Elemento/seletor ${elemento} não encontrado`);
+  }
+}
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    const codigo = evento.code;
+    if (codigo === "Space" || codigo === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
